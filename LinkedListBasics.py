17aa0d119722f7f8c98c07cd3a095f3d8e1a961d
bhavyabgada <bhavyabgada@gmail.com>
2021-12-02 15:31:48 +0530

    append method

--- LinkedListBasics.py
+++ LinkedListBasics.py
@@ -11,6 +11,15 @@ class LinkedList(object):
     def __init__(self, head=None):
         self.head = head
 
+    #Adding Element at the end of the Linked List
 
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
     
 
