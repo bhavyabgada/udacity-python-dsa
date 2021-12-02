434bdcda5c1d86e90f839049eb6a55e01c3a5c3a
bhavyabgada <bhavyabgada@gmail.com>
2021-12-02 15:31:14 +0530

    Initial Linked list

---
+++ LinkedListBasics.py
@@ -0,0 +1,16 @@
+#Single Unit Linked List
+
+class Element(object):
+    def __init__(self, value):
+        self.value = 'value'
+        self.next = None
+
+#Linked List Class
+
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+
+
+    
+
